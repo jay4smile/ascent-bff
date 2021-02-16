1ebd8e4ce5326e1b309bdd9e1bb73bab00842ac2
NoeSamaille <samaille.noe@gmail.com>
2021-02-16 17:40:24 +0100

    NIST Table

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,6 +1,7 @@
 export * from './ping.controller';
 export * from './health.controller';
 export * from './controls.controller';
+export * from './nist.controller';
 export * from './families.controller';
 export * from './services.controller';
 export * from './automation-catalog.controller';
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,5 @@
 export * from './controls.model';
+export * from './nist.model';
 export * from './architectures.model';
 export * from './families.model';
 export * from './bom.model';
