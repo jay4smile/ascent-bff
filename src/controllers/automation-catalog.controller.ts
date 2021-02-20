9a6ddbcbd5cdc3f75fe3974600c4ebda1a6f37ef
Github pipeline <pipeline@github.com>
2021-02-19 19:13:53 -0600

    Update BOMBuilder

--- src/controllers/automation-catalog.controller.ts
+++ src/controllers/automation-catalog.controller.ts
@@ -11,12 +11,43 @@ export class AutomationCatalogController {
     // Retrieve the Catalog and convert it to JSON
     // https://raw.githubusercontent.com/ibm-garage-cloud/garage-terraform-modules/gh-pages/index.yaml
 
+    // Load the Catalog using the Catalog Class
+
+    // Then output the Catalog in JSON
+
 
     return {'status':"UP"};
 
 
 
   }
+
+  @get('/automation/{bomid}')
+  automationBuilder(): object {
+
+    // Read Architecture Bill of Materials
+
+    // Load Service Map to the Bill Materials
+
+    // Load Catalog
+
+    // Map Builder BOM to Automation Catalog creating a Automation BOM
+
+    // Build Terraform Components
+
+    // Write into a Buffer
+
+    // Build Zipfile
+
+    https://www.archiverjs.com/zip-stream/
+
+    // Stream Buffer back
+
+    // Future : Push to Object Store, Git, Create a Tile Dynamically
+
+        return {'status':"UP"};
+  }
+
   @get('/catalog/{id}')
   catalogId(): object {
 
