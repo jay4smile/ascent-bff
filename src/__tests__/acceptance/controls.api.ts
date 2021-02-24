69681fa412d5400d7cb74eadf0fb0e90c4bfab2e
NoeSamaille <noe.samaille@protonmail.com>
2021-02-24 17:16:36 +0100

    Cleaner test framework

--- src/__tests__/acceptance/controls.api.ts
+++ src/__tests__/acceptance/controls.api.ts
@@ -1,22 +1,23 @@
-import {Client} from '@loopback/testlab';
-import {ArchitectureMapperBffApplication} from '../..';
-import {setupApplication} from './test-helper';
+import { Client } from '@loopback/testlab';
+import { ArchitectureMapperBffApplication } from '../..';
+import { setupApplication } from './test-helper';
 
 /* eslint-disable @typescript-eslint/naming-convention */
 
 describe('Controls', () => {
   let app: ArchitectureMapperBffApplication;
   let client: Client;
+  const testControlId = "TEST-1"
 
   before('setupApplication', async () => {
-    ({app, client} = await setupApplication());
+    ({ app, client } = await setupApplication());
   });
 
   after(async () => {
     await app.stop();
   });
 
-  it('Get controls count', async () => {
+  it('GET controls count', async () => {
     await client
       .get('/controls/count')
       .expect(200)
@@ -24,17 +25,17 @@ describe('Controls', () => {
       .expect(/{"count":\d+}/gm)
   });
 
-  it('Get all controls', async () => {
+  it('GET all controls', async () => {
     await client
       .get('/controls')
       .expect(200)
       .expect('Content-Type', /application\/json/);
   });
 
-  it('POST and DELETE a control', async () => {
+  it('POST a control', async () => {
     await client
       .post('/controls').send({
-        "control_id": "TEST-1",
+        "control_id": testControlId,
         "control_family": "test control_family",
         "cf_description": "test cf_description",
         "base_control": true,
@@ -50,9 +51,39 @@ describe('Controls', () => {
       })
       .expect(200)
       .expect('Content-Type', /application\/json/);
-      await client
-      .delete('/controls/TEST-1')
+  });
+
+  it('GET control by id', async () => {
+    await client
+      .get(`/controls/${testControlId}`)
+      .expect(200)
+      .expect('Content-Type', /application\/json/);
+  });
+
+  it('PATCH a control', async () => {
+    await client
+      .patch(`/controls/${testControlId}`).send({
+        "comment": "test comment updated"
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect(/{.*"comment":"test comment updated".*}/);
+  });
+
+  it('DELETE a control', async () => {
+    await client
+      .delete(`/controls/${testControlId}`)
       .expect(204);
+  });
+
+  it('PATCH all controls', async () => {
+    await client
+      .patch(`/controls`).send({
+        "comment": "test comment updated"
+      })
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+      .expect(/{"count":\d+}/gm);
   });
 
 });
