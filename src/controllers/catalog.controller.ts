1f2f5c18d6207395d7b0122094b2dee784ed0509
Yogendra Srivastava <ysrivastava@in.ibm.com>
2021-03-05 19:06:26 +0530

    redis

--- src/controllers/catalog.controller.ts
+++ src/controllers/catalog.controller.ts
@@ -1,29 +1,45 @@
 import {  
     param,
     get,  
-    response,
+    response,      
   } from '@loopback/rest';
-  import fetch from 'cross-fetch';
-  
+import { request } from 'express';
+import path from 'path';
+  //import fetch from 'cross-fetch';
+  import fetch from 'node-fetch';
+  import * as redis from "redis";
+import { promisify } from 'util';
+import { any } from 'nconf';
+//import { Request, get } from '@loopback/rest';
+//import { request } from 'express';
   export class CatalogController {
-    @get('/catalog')
-    @response(200, {
-      description: 'Catalog',
-      content: 'application/json'
-    })
-    async catalog(): Promise<JSON> {    
-         
-      const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false');       
-      const res = await fetch( url.toString() );
-       
-      if (res.status >= 400) {
-        throw new Error("Bad response from server");
-      }
-  
-       const data = await res.json();        
-       return data;
-    }
-  
+    
+    
+    @get('/catalog')    
+    async catalog(): Promise<any> { 
+      
+      var redisClient = redis.createClient(6379, "127.0.0.1");  
+      const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false');      
+      const key = "catalog1";
+        
+      return redisClient.get(key, (error: any, data: any) => {          
+         if (data) {           
+           console.log("***yks****"+data);                 
+           return data;        
+         } else{           
+           return fetch(url.toString())
+            .then(response => response.json())
+            .then(data => {            
+            console.log("****data*****"+data);
+            //redisClient.set("catalog",data);
+            return data;
+      }).catch(err => {        
+         return err;
+       });
+     }    
+   });  
+  }
+
   
     @get('/catalog/{id}')
     @response(200, {
@@ -32,16 +48,14 @@ import {
     })
     async catalogById(
       @param.path.string('id') id: string    
-    ): Promise<JSON> {
-      
+    ): Promise<JSON> {      
       const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false&q='+id);      
       const res = await fetch( url.toString() );
        
       if (res.status >= 400) {
         throw new Error("Bad response from server");
-      }
-  
-       const data = await res.json();        
+      }      
+      const data = await res.json();             
        return data;    
     }  
   
