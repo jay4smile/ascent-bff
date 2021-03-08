6656ec739a905c8122d83fbdd5482fcbc60cb159
Github pipeline <pipeline@github.com>
2021-03-08 15:35:52 -0600

    Add some slightly different logic

--- src/controllers/catalog.controller.ts
+++ src/controllers/catalog.controller.ts
@@ -1,63 +1,73 @@
-import {  
+import {
     param,
-    get,  
-    response,      
-  } from '@loopback/rest';
-import { request } from 'express';
+    get,
+    response, RestBindings, Response,
+} from '@loopback/rest';
 import path from 'path';
-  //import fetch from 'cross-fetch';
-  import fetch from 'node-fetch';
-  import * as redis from "redis";
-import { promisify } from 'util';
+import fetch from 'node-fetch';
+import * as redis from "redis";
 import { any } from 'nconf';
-//import { Request, get } from '@loopback/rest';
-//import { request } from 'express';
-  export class CatalogController {
-    
-    
-    @get('/catalog')    
-    async catalog(): Promise<any> { 
-      
-      var redisClient = redis.createClient(6379, "127.0.0.1");  
-      const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false');      
-      const key = "catalog1";
-        
-      return redisClient.get(key, (error: any, data: any) => {          
-         if (data) {           
-           console.log("***yks****"+data);                 
-           return data;        
-         } else{           
-           return fetch(url.toString())
-            .then(response => response.json())
-            .then(data => {            
-            console.log("****data*****"+data);
-            //redisClient.set("catalog",data);
-            return data;
-      }).catch(err => {        
-         return err;
-       });
-     }    
-   });  
-  }
+import {inject} from "@loopback/core";
+export class CatalogController {
 
-  
+    @get('/catalog')
+    async catalog(
+        @inject(RestBindings.Http.RESPONSE) res: Response,
+    ): Promise<any> {
+
+        // Fix this to be retrieved from the Environment
+        var client =  redis.createClient(6379, "127.0.0.1");
+        const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false');
+        const key = "catalog";
+
+        //log error to the console if any occurs
+        client.on("error", (err) => {
+            console.log(err);
+        });
+
+        try {
+            client.get(key, async (err, data) => {
+                if (err) throw err;
+
+                if (data) {
+                    console.log("data retrieved from the cache");
+                    res.status(200).send(JSON.parse(data));
+                } else {
+                    fetch(url).then(data=> {
+                        if(data.json()) {
+                            client.setex(key, 28800, JSON.stringify(data.json()));
+                            console.log(data.json())
+                            console.log("cache miss");
+                            res.status(200).send(data.json());
+                        } else {
+                            res.status(404).send({message: "no data found"});
+                        }
+                    });
+                }
+            });
+        } catch(err) {
+          res.status(500).send({message: err.message});
+        }
+
+   }
+
     @get('/catalog/{id}')
     @response(200, {
       description: 'Catalog by id',
       content: 'application/json'
     })
     async catalogById(
-      @param.path.string('id') id: string    
-    ): Promise<JSON> {      
-      const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false&q='+id);      
-      const res = await fetch( url.toString() );
-       
+        @param.path.string('id') id: string,
+    ): Promise<JSON> {
+
+      const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false&q='+id);
+      const res  = await fetch( url.toString() );
+
       if (res.status >= 400) {
         throw new Error("Bad response from server");
-      }      
-      const data = await res.json();             
-       return data;    
-    }  
-  
+      }
+      const data = await res.json();
+      return data;
+    }
+
   }
-  
