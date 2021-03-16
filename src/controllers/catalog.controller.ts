9f5aea1189e591ebaeeb23c59253ac9de01dd306
Yogendra Srivastava <ysrivastava@in.ibm.com>
2021-03-16 17:30:27 +0530

    redis cache

--- src/controllers/catalog.controller.ts
+++ src/controllers/catalog.controller.ts
@@ -6,7 +6,7 @@ import {
 } from '@loopback/rest';
 import fetch from 'node-fetch';
 import { Tedis, TedisPool } from "tedis";
-import { inject } from "@loopback/core";
+
 
 
 /* eslint-disable @typescript-eslint/no-explicit-any */
@@ -15,12 +15,11 @@ export class CatalogController {
 
   @get('/catalog/{offset}/{limit}')
   async catalog(
-    //@inject(RestBindings.Http.RESPONSE) res: Response,
     @param.path.string('offset') offset: string,
     @param.path.string('limit') limit: string,
   ): Promise<JSON> {
 
-    const url = new URL("https://globalcatalog.cloud.ibm.com/api/v1?_offset="+offset+"&_limit="+limit+"&complete=false");
+    const url = new URL("https://globalcatalog.cloud.ibm.com/api/v1?_offset=" + offset + "&_limit=" + limit + "&complete=false");
     const res = await fetch(url.toString());
 
     if (res.status >= 400) {
@@ -29,7 +28,7 @@ export class CatalogController {
     const data = await res.json();
     return data;
   }
-  
+
 
   @get('/catalog/{id}')
   @response(200, {
@@ -38,16 +37,35 @@ export class CatalogController {
   })
   async catalogById(
     @param.path.string('id') id: string,
-  ): Promise<JSON> {
+  ): Promise<any> {
 
-    const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false&q=' + id);
-    const res = await fetch(url.toString());
+    let tedis = new Tedis({
+      port: 6379,
+      host: "127.0.0.1"
+    });
 
-    if (res.status >= 400) {
-      throw new Error("Bad response from server");
+    const jsonobj = [];
+    const key = "catalog";
+
+    try {
+
+      if (await tedis.exists(id) !== 0) {
+        const data = await tedis.get(id);
+        console.log("data retrieved from the cache");
+        jsonobj.push(data);
+      } else {
+        const url = new URL('https://globalcatalog.cloud.ibm.com/api/v1?_limit=100&complete=false&q=' + id);
+        const res = await fetch(url.toString());
+        const data = await res.json();
+        await tedis.set(id, JSON.stringify(data));
+        console.log("cache miss");
+        jsonobj.push(data);
+      }
+    } catch (error) {
+      throw error;
     }
-    const data = await res.json();
-    return data;
+
+    return jsonobj;
   }
 
 }
