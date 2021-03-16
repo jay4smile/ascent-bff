cec414f82ceada0e5d08b71f5984f8422386dff0
Noé Samaille <noe.samaille@ibm.com>
2021-03-16 10:51:50 +0100

    POST control mapping

---
+++ data/dbdocs/initDatabase.js
@@ -0,0 +1,26 @@
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+
+mongodbServices = JSON.parse(process.env.DATABASE_TEST)
+const mongodbConn = mongodbServices.connection.mongodb;
+const mongodbComposed = mongodbConn.composed[0];
+const ca = [Buffer.from(mongodbConn.certificate.certificate_base64, 'base64')];
+
+const testdb = "ibmcloudtestdb";
+const maindb = "ibmclouddb";
+
+MongoClient.connect(mongodbComposed, { ssl: true, sslCA: ca }, (err, client) => {
+
+    if (err) throw err;
+
+    console.log(client.topology.clientInfo);
+
+    let db = client.db(testdb);
+    db.collection('ControlMapping').createIndex( { control_id: 1, service_id: 1 }, { unique: true } );
+    db.collection('ControlMapping').createIndex( { control_id: 1, arch_id: 1 }, { unique: true } );
+    db = client.db(maindb);
+    db.collection('ControlMapping').createIndex( { control_id: 1, service_id: 1 }, { unique: true } );
+    db.collection('ControlMapping').createIndex( { control_id: 1, arch_id: 1 }, { unique: true } );
+
+    client.close();
+});
