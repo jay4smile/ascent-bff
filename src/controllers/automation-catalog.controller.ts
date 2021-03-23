3b8e3d6cf93724c83adef245ca6b1bdedc56a6f2
Github pipeline <pipeline@github.com>
2021-03-23 17:25:50 -0500

    Update data models

--- src/controllers/automation-catalog.controller.ts
+++ src/controllers/automation-catalog.controller.ts
@@ -120,8 +120,8 @@ export class AutomationCatalogController  {
     // Future : Push to Object Store, Git, Create a Tile Dynamically
     const bom: BillOfMaterialModel = new BillOfMaterial("fscloud");
 
-    bom.spec.modules.push("github.com/ibm-garage-cloud/terraform-k8s-ocp-cluster");
-    bom.spec.modules.push("github.com/ibm-garage-cloud/terraform-ibm-appid")
+    bom.spec.modules.push("github.com/ibm-garage-cloud/terraform-ibm-container-platform");
+    //bom.spec.modules.push("github.com/ibm-garage-cloud/terraform-ibm-appid")
 
     // From the BOM build an Automation BOM
     automationBom.forEach(_bom => {
@@ -142,9 +142,7 @@ export class AutomationCatalogController  {
     })
 
     //const filter: {platform?: string; provider?: string} =  {}; // SS explain ?
-
     //const billOfMaterial: BillOfMaterialModel =  await this.moduleSelector.buildBillOfMaterial(catalog, bom, filter) ;
-
     //if (!billOfMaterial) {
     //  throw new Error('Bill of Material is required');
     //}
