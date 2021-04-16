4904119f4f7006ad8969d98c4aa4c15cd4752f2b
Github pipeline <pipeline@github.com>
2021-04-15 19:42:54 -0500

    Updates for iascable version

--- src/controllers/automation-catalog.controller.ts
+++ src/controllers/automation-catalog.controller.ts
@@ -196,8 +196,7 @@ export class AutomationCatalogController  {
     })
 
     if (!_.isEmpty(_errors)) {
-      res.status(409);
-      res.send(_errors);
+      return res.status(409).send(_errors);
       return;
     }
 
@@ -209,7 +208,9 @@ export class AutomationCatalogController  {
 
       // Lets build a BOM file from the BOM builder
       const bomContents: string = jsYaml.dump(bom);
+      console.log('=====');
       console.log(bomContents);
+      console.log('=====');
 
       const modules: SingleModuleVersion[] = await this.moduleSelector.resolveBillOfMaterial(this.catalog, bom);
       const terraformComponent: TerraformComponent = await this.terraformBuilder.buildTerraformComponent(modules);
@@ -290,9 +291,7 @@ export class AutomationCatalogController  {
 
     } catch (e) {
       console.log(e);
-      res.status(409);
-      res.send(e.message);
-      return;
+      return res.status(409).send(e.message);
     }
 
   }
